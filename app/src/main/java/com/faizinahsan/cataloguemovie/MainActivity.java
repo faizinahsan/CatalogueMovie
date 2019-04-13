6cb8a889767c62d40f7e171152bc41c06bff3a5b
faizinahsan <faizin110998@gmail.com>
2019-04-13 12:41:32 +0700

    Provider sudah bisa add & delete favorite sama tv juga bisa

--- app/src/main/java/com/faizinahsan/cataloguemovie/MainActivity.java
+++ app/src/main/java/com/faizinahsan/cataloguemovie/MainActivity.java
@@ -1,10 +1,13 @@
 package com.faizinahsan.cataloguemovie;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.faizinahsan.cataloguemovie.fragment.FavoritesFragment;
@@ -42,5 +45,19 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null){
             navigationView.setSelectedItemId(R.id.navigation_home);
         }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_change_settings){
+            Intent mIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(mIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
